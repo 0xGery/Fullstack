c9adb1ffb0c2f67a5328d40e558e64a720f15382
scryotic-tech <mrswagery@gmail.com>
2023-12-04 19:30:49 +0700

    -Backend module -Server backup

---
+++ Backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3001; // Different from the React port
+const mongoose = require('mongoose');
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
+mongoose.connect('mongodb://localhost:27017/myPortfolio', { useNewUrlParser: true, useUnifiedTopology: true });
