88be6ddc456a297833a56c897fb1c77c3cde2c5c
scryotic-tech <mrswagery@gmail.com>
2023-12-06 17:58:00 +0700

    a

---
+++ Frontend/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <p>© 2023 0xGery. All Right reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
