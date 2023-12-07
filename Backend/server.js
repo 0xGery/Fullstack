4e10db7a6f55be8aa8b1e2e9ba13fe6cf95cc670
scryotic-tech <mrswagery@gmail.com>
2023-12-07 16:14:28 +0700

    a

--- Backend/server.js
+++ Backend/server.js
@@ -6,18 +6,24 @@ const port = 3001;
 const cors = require('cors');
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/0xGery', { useNewUrlParser: true, useUnifiedTopology: true });
+const dbUsername = '0xGery'; // Replace with your MongoDB username
+const dbPassword = 'Ab1CdFo12'; // Replace with your MongoDB password
+const dbHost = '93.188.161.44';
+const dbName = '0xGery';
+const connectionString = `mongodb://${dbUsername}:${encodeURIComponent(dbPassword)}@${dbHost}:27017/${dbName}`;
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
 
 // Middleware to parse JSON requests
 app.use(express.json());
 
-// Test route
-app.get('/', (req, res) => res.send('Backend server is running!'));
-
 app.use(cors()); // CORS middleware
 app.use('/api', projectRoutes); // API routes
 
+// Test route
+app.get('/', (req, res) => res.send('Backend server is running!'));
 
 // Start the server
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
