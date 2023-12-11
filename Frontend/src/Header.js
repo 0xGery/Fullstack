f46d985d7b13a2e69df5a83ddb3110277e4d4a6c
scryotic-tech <mrswagery@gmail.com>
2023-12-11 17:40:23 +0700

    Add Docs link

--- Frontend/src/Header.js
+++ Frontend/src/Header.js
@@ -27,7 +27,7 @@ function Header() {
                         <a href="#Service" onClick={() => handleNavLinkClick('Service')} className={activeLink === 'Service' ? 'selected' : ''}>Service</a>
                         <a href="#Team" onClick={() => handleNavLinkClick('Team')} className={activeLink === 'Team' ? 'selected' : ''}>Team</a>
                         <a href="#Contact" onClick={() => handleNavLinkClick('Contact')} className={activeLink === 'Contact' ? 'selected' : ''}>Contact</a>
-                        <a href="#Documentation" onClick={() => handleNavLinkClick('Documentation')} className={activeLink === 'Documentation' ? 'selected' : ''}>Documentation</a>
+                        <a href="https://docs.0xgery.xyz" target="_blank" onClick={() => handleNavLinkClick('Documentation')} className={activeLink === 'Documentation' ? 'selected' : ''}>Documentation</a>
                     </li>
                     <li className="topnav-right"><a href="#connect-section" onClick={() => handleNavLinkClick('ConnectWallet')} className={activeLink === 'ConnectWallet' ? 'selected' : ''}>Connect Wallet</a></li>
                     <li className="dropdownIcon">
