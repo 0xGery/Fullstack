fde362f08e951ebf2682532f275717ead1460bd2
scryotic-tech <mrswagery@gmail.com>
2023-12-21 23:48:22 +0700

    test

--- Frontend/src/WelcomeSection.js
+++ Frontend/src/WelcomeSection.js
@@ -9,7 +9,7 @@ function WelcomeSection() {
             <div className="rowW">
                 <div className="col-6"> 
                     <div className="leftSide-col">
-                        <h1 className="large">NOCTURNNODE</h1>
+                        <h1 className="large">NOCTURN NODE</h1>
                         <h1 className="large2">Nocturn is a team of accomplished validators with a strong focus on blockchain architecture. We deliver relayer services and contribute actively to the growth of various blockchain projects.</h1>
                         <h1 className="large2">Our primary aim is to enable communities to engage meaningfully with projects they are enthusiastic about. We provide complimentary tutorials that guide users on how to become active contributors to their selected projects. Although we offer our services free of charge, we plan to introduce features for those who wish to support our team monetarily. Furthermore, those interested in delegating to our validator can look forward to the 'Connect Wallet' feature, scheduled to be available soon.</h1>
                         <h1 className="large2">Founded in 2021, Nocturn had its fair share of funding challenges that initially slowed our development. Fast-forward to 2024, and we have fully assembled our team. Resembling nocturnal bats on the hunt, we are prepared to excel in the blockchain ecosystem.</h1>
